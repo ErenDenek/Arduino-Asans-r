fa71ab7b74f1fd21879fd1e0ec50a14b6ebe9dc7
ErenDenek <erenwtp09@gmail.com>
2017-05-09 13:39:11 +0300

    Update asnsor.ino

--- asnsor.ino
+++ asnsor.ino
@@ -1,208 +1,132 @@
 #include <Servo.h>
-#define a 20
-#define b 21
-#define c 22
-#define d 23
-#define e 24
-#define f 25
-#define g 26
-
-bool buton1 = 0, buton2 = 0, buton3 = 0, x = 0, buton = 0;
-int ldr1 = 0, ldr2 = 0, ldr3 = 0;
-int Durum1, Durum2, Durum3, kat = 0;
-
-int Durum[3];
-
-int output[10] = {a, b, c, d, e, f, g, 6, 5, 4};
-int input[6] = {2, 8, 9, A0, A2, A3};
+int a = 24, b = 25, c = 27, d = 28, e = 29, f = 23, g = 22,x;
+int ldr1 = 0, ldr2 = 0, ldr3 = 0, kat = 0, Durum1, Durum2, Durum3;
+int output[20] = {a, b, c, d, e, f, g, 6, 5, 4, 22, 23, 24, 25, 29, 27, 28, 11, 12};
+int input[6] = {8, 9, 10, A0, A2, A3};
+int State[3] = {Durum1, Durum2, Durum3};
+unsigned long zaman1 = 0, zaman2, zaman3, zaman4, zaman;
 
 Servo myservo1;
 Servo myservo2;
 Servo myservo3;
 
-unsigned long zaman1 , zaman2, zaman3, zaman4, zaman;
-
 void setup() {
-  Serial.begin(250000);
-
   myservo1.attach(12);
-  myservo2.attach(10);
+  myservo2.attach(13);
   myservo3.attach(11);
-
-  for (int i = 0; i <= 9; i++)pinMode(output[i], OUTPUT);
+  for (int i = 0; i <= 20; i++)pinMode(output[i], OUTPUT);
   for (int j = 0; j <= 5; j++)pinMode(input[j], INPUT);
-
 }
-
 void loop() {
-
-  Durum1 = but(2, 1);
-  Durum2 = but(8, 2);
-  Durum3 = but(9, 3);
+  State[0] = button(8, 1);
+  State[1] = button(9, 2);
+  State[2] = button(10, 3);
   sens();
   katSegment();
-
   zaman = millis();
-
-  while (Durum2 == 1) {
-
+  while (State[2] == 1) {
 flag2:
     zaman = millis();
     sens();
-
-    if (kat == 1 )up();
-    if (kat == 3 )down();
-    if (kat != 2)goto flag2;
-
+    if (kat == 1 ) up();
+    if (kat == 3 ) down();
+    if (kat != 2) goto flag2;
     while (kat == 2) {
-
-      Serial.println("SERVO ON");
       myservo1.write(180);
-
       if (millis() - zaman > 3000) {
-
         zaman = millis();
-        Serial.println("SERVO OFF");
         myservo1.write(0);
-        Durum2 = 0;
+        State[2] = 0;
         break;
-
       }
     }
   }
-
-  while (Durum1 == 1)  {
-
+  while (State[1] == 1)  {
 flag1:
-    
     zaman = millis();
     sens();
-    
+    katSegment();
     if (!(kat == 1))down();
     if (kat != 1)goto flag1;
-    
     while (kat == 1)  {
-      Serial.println("SERVO ON");
       myservo2.write(180);
       if (millis() - zaman > 3000)  {
-        
         zaman = millis();
-        Serial.println("SERVO OFF");
         myservo2.write(0);
-        Durum1 = 0;
+        State[1] = 0;
         break;
-      
       }
     }
   }
-  
-  while (Durum3 == 1){
-
+  while (State[3] == 1) {
 flag3:
     zaman = millis();
     sens();
-
     if (!(kat == 3))up();
     if (kat != 3)goto flag3;
-    
-    while (kat == 3){
-      
-      Serial.println("SERVO ON");
+    while (kat == 3) {
       myservo3.write(180);
-      
       if (millis() - zaman > 3000)  {
         zaman = millis();
-        Serial.println("SERVO OFF");
         myservo3.write(0);
-        Durum3 = 0;
+        State[3] = 0;
         break;
-      
       }
     }
-  }  
-  
-  Serial.print(kat); Serial.println(".KATTA Durdu");
-
+  }
 }
-
 void up()
 {
   digitalWrite(6, LOW);
   digitalWrite(4, HIGH);
-  
-  if (millis() - zaman1 > 1){
-    
+  if (millis() - zaman1 > 1) {
     digitalWrite(5, HIGH);
     zaman1 = millis();
   }
-  
-  if (millis() - zaman2 > 1){
-    
+  if (millis() - zaman2 > 1) {
     digitalWrite(5, LOW);
     zaman2 = millis();
   }
-
-  Serial.println("UP");
-
+  katSegment();
 }
-
 void down()
 {
   digitalWrite(6, LOW);
   digitalWrite(4, LOW);
+  if (millis() - zaman3 > 1) {
 
-  if (millis() - zaman3 > 1){
-    
     digitalWrite(5, HIGH);
     zaman3 = millis();
   }
-  if (millis() - zaman4 > 1){
-    
+  if (millis() - zaman4 > 1) {
     digitalWrite(5, LOW);
     zaman4 = millis();
   }
-  
-  Serial.println("down");  
+  katSegment();
 }
-
 void sens()
 {
-
   ldr1 = analogRead(A1);
   ldr2 = analogRead(A2);
-  ldr3 = analogRead(A0);
-  
-  if (ldr1 <= 700)kat = 3;
-  if (ldr2 <= 800)kat = 2;
-  if (ldr3 <= 700)kat = 1;
-
-
+  ldr3 = analogRead(A3);
+  if (ldr1 <= 890)kat = 3;
+  if (ldr2 <= 900)kat = 2;
+  if (ldr3 <= 900)kat = 1;
 }
-void button(int pin)
+int button(int pin, int durum)
 {
-  
+  int Durum[3];
   if (digitalRead(pin) == 1 && x == 0) {
     x = 1;
-    if (Durum[pin] == 0) Durum[pin] = 1;
-    else if (Durum[pin] == 1) Durum[pin] = 0;
+    if (Durum[durum] == 0) Durum[durum] = 1;
+    else if (Durum[durum] == 1) Durum[durum] = 0;
   }
-  else if (digitalRead(pin) == 0 && x == 1) {
-    x = 0;
-  }
-  Serial.print(Durum[pin]);
-  Serial.println();
+  else if (digitalRead(pin) == 0 && x == 1)x = 0;
+  return Durum[durum];
 }
 void katSegment()
 {
-  
-  digitalWrite(a, LOW);
-  digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
-  digitalWrite(d, LOW);
-  digitalWrite(e, LOW);
-  digitalWrite(f, LOW);
-  digitalWrite(g, LOW);
-  
+  for (int k = 0; k <= 6; k++)digitalWrite(k, LOW);
   switch (kat)
   {
     case 1:
@@ -210,27 +134,12 @@ void katSegment()
       digitalWrite(d, HIGH);
       digitalWrite(e, HIGH);
       digitalWrite(f, HIGH);
-      digitalWrite(g, HIGH); break;
+      digitalWrite(g, HIGH);  break;
     case 2:
       digitalWrite(c, HIGH);
       digitalWrite(f, HIGH);  break;
     case 3:
       digitalWrite(e, HIGH);
-      digitalWrite(f, HIGH);   break;
+      digitalWrite(f, HIGH);  break;
   }
-}
-
-void but(int but_pin, int but_kat)
-{
-  if (digitalRead(but_pin) == 1 && x == 0) {
-    x = 1;
-    if (Durum[but_kat] == 0) Durum[but_kat] = 1;
-    else if (Durum[but_kat] == 1) Durum[but_kat] = 0;
-  }
-  else if (digitalRead(but_pin) == 0 && x == 1) {
-    x = 0;
-  }
-
-  return Durum[but_kat];
-
 }
