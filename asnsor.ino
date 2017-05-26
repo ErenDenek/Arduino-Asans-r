c7c26d70da8c056a8f2ff936d17002f865bc0583
ErenDenek <erenwtp09@gmail.com>
2017-05-26 23:04:25 +0300

    Update asnsor.ino

--- asnsor.ino
+++ asnsor.ino
@@ -1,4 +1,8 @@
 #include <Servo.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 int a = 41;
 int b = 43;
 int c = 45;
@@ -6,12 +10,12 @@ int d = 35;
 int e = 33;
 int f = 39;
 int g = 37;
-int x;
+int x2, x1, x3, x4, x5, x6;
 int ldr1 = 0, ldr2 = 0, ldr3 = 0, kat = 0, Durum1, Durum2, Durum3, acil_count = 0, servis_count = 0, Kabin1, Kabin2, Kabin3, sayac = 0;
-int output[20] = {a, b, c, d, e, f, g, 6, 5, 4, 22, 23, 24, 25, 29, 27, 28, 11};
-int input[11] = {53, 51, 49, 10, 9, 12, A0, A2, A3, 2, 3};
-int State[3] = {Durum1, Durum2, Durum3};
-unsigned long zaman1 = 0, zaman2, zaman3, zaman4, zaman;
+int output[15] = {a, b, c, d, e, f, g, 11, 4, 5, 6, 7, 8, 13};
+int input[12] = {53, 51, 49, 10, 9, 12, A0, A2, A3, 2, 3};
+
+unsigned long lcdzaman = 0, lcdzaman1 = 0, zaman1 = 0, zaman2, zaman3, zaman4, zaman;
 
 Servo myservo1;
 Servo myservo2;
@@ -20,48 +24,54 @@ Servo myservo3;
 void setup() {
   // put your setup code here, to run once:
 
-  for (int i = 0; i <= 20; i++)pinMode(output[i], OUTPUT);
-  for (int j = 0; j <= 11; j++)pinMode(input[j], INPUT);
+  for (int i = 0; i <= 14; i++)pinMode(output[i], OUTPUT);
+  for (int j = 0; j <= 12; j++)pinMode(input[j], INPUT);
 
-  myservo1.attach(18);
-  myservo2.attach(13);
+  myservo1.attach(13);
+  myservo2.attach(8);
   myservo3.attach(11);
 
   attachInterrupt(digitalPinToInterrupt(2), acil, RISING);
   attachInterrupt(digitalPinToInterrupt(3), servis, RISING);
-
-  Serial.begin(250000);
-
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-  oku();
 
-  Serial.print(Kabin1); Serial.print("  ");
-  Serial.print(Kabin2); Serial.print("  ");
-  Serial.print(Kabin3); Serial.print("   ");
-  Serial.print(ldr1); Serial.print("  ");
-  Serial.print(ldr2); Serial.print("  ");
-  Serial.print(ldr3); Serial.print("  ");
-  Serial.print(acil_count); Serial.print("  ");
+  myservo1.write(0);
+  myservo2.write(0);
+  myservo3.write(0);
+
+  oku();
   sens();
   katSegment();
+
   zaman = millis();
+
+
   while (Durum2 == 1) {
 flag2:
     if (acil_count > 0)goto em;
     zaman = millis();
     sens();
+    oku();
     katSegment();
     if (kat == 1 ) up();
     if (kat == 3 ) down();
     if (kat != 2) goto flag2;
     while (kat == 2) {
       if (acil_count > 0)goto em;
-      myservo1.write(180);
+      lcd.print("KAPI ACILIYOR");
+      delay(60);
+      lcd.clear();
+      myservo1.write(90);
+      oku();
+      tone(7, 1000);
 
-      Serial.println("2. Kat Servo On");
+
       if (millis() - zaman > 3000) {
+        oku();
         zaman = millis();
         myservo1.write(0);
         sayac++;
@@ -72,21 +82,39 @@ flag2:
   }
   while (Durum3 == 1)  {
 flag1:
+aa:
+while(!(ldr1<890))
+{
+  sens();
+  katSegment();
+  down();
+  acil_count=0;
+}
+
+
     if (acil_count > 0)goto em;
     zaman = millis();
     sens();
+    oku();
     katSegment();
     if (!(kat == 1))down();
     if (kat != 1)goto flag1;
-    while (kat == 1)  {
+    while (kat == 1) {
       if (acil_count > 0)goto em;
-      myservo2.write(180);
-      Serial.println("1.Kat Servo On");
+      lcd.print("KAPI ACILIYOR");
+      delay(60);
+      lcd.clear();
+      myservo2.write(90);
+      oku();
+      tone(7, 1000);
+
+
       if (millis() - zaman > 3000)  {
+        oku();
         zaman = millis();
         myservo2.write(0);
-        sayac++;
         Durum3 = 0;
+        sayac++;
         break;
       }
     }
@@ -96,15 +124,24 @@ flag3:
     if (acil_count > 0)goto em;
     zaman = millis();
     sens();
+    oku();
     katSegment();
     if (!(kat == 3))up();
     if (kat != 3)goto flag3;
     while (kat == 3) {
       if (acil_count > 0)goto em;
-      myservo3.write(180);
-      Serial.println("3.Kat Servo On");
+      lcd.print("KAPI ACILIYOR");
+      delay(60);
+      lcd.clear();
+      myservo3.write(90);
+      oku();
+      tone(7, 1000);
+
+
       if (millis() - zaman > 3000)  {
+        oku();
         zaman = millis();
+        myservo3.write(0);
         myservo3.write(0);
         Durum1 = 0;
         sayac++;
@@ -115,6 +152,7 @@ flag3:
   while (Kabin1 == 1) {
 flag4:
     if (acil_count > 0)goto em;
+    oku();
     zaman = millis();
     sens();
     katSegment();
@@ -123,10 +161,18 @@ flag4:
     if (kat != 1)goto flag4;
     while (kat == 1) {
       if (acil_count > 0)goto em;
-      myservo3.write(180);
-      Serial.println("3.Kat Servo On");
+      lcd.print("KAPI ACILIYOR");
+      delay(60);
+      lcd.clear();
+      myservo2.write(90);
+      tone(7, 1000);
+      oku();
+
+
       if (millis() - zaman > 3000)  {
+        oku();
         zaman = millis();
+        myservo2.write(0);
         myservo3.write(0);
         Kabin1 = 0;
         sayac++;
@@ -139,15 +185,22 @@ flag5:
     if (acil_count > 0)goto em;
     zaman = millis();
     sens();
+    oku();
     katSegment();
     if (kat == 1 ) up();
     if (kat == 3 ) down();
     if (kat != 2) goto flag5;
     while (kat == 2) {
       if (acil_count > 0)goto em;
-      myservo1.write(180);
-      Serial.println("2. Kat Servo On");
+      lcd.print("KAPI ACILIYOR");
+      delay(60);
+      lcd.clear();
+      myservo1.write(90);
+      tone(7, 1000);
+      oku();
+
       if (millis() - zaman > 3000) {
+        oku();
         zaman = millis();
         myservo1.write(0);
         Kabin2 = 0;
@@ -162,16 +215,24 @@ flag6:
     zaman = millis();
     sens();
     katSegment();
+    oku();
     if (!(kat == 3))up();
     if (kat != 3)goto flag6;
     while (kat == 3)
     {
       if (acil_count > 0)goto em;
-      myservo2.write(180);
-      Serial.println("1.Kat Servo On");
+      lcd.print("KAPI ACILIYOR");
+      delay(60);
+      lcd.clear();
+      myservo3.write(90);
+      tone(7, 1000);
+      oku();
+
+
       if (millis() - zaman > 3000)  {
+        oku();
         zaman = millis();
-        myservo2.write(0);
+        myservo3.write(0);
         Kabin3 = 0;
         sayac++;
         break;
@@ -179,10 +240,25 @@ flag6:
     }
   }
 em:
-  emerg();
-  while (sayac >= 3)
+if(acil_count>0)
+{
+  Durum1=0;
+  Durum2=0;
+  Durum3=0;
+  Kabin1=0;
+  Kabin2=0;
+  Kabin3=0;
+  goto aa;
+}
+  while (sayac >= 10)
   {
-    Serial.println("bakim");
+    lcd.print("BAKIM ZAMANI");
+    delay(1000);
+    lcd.clear();
+    lcd.print("YETKILIYI CAGIRINIZ");
+    delay(1000);
+    lcd.clear();
+
     if (servis_count > 1)
     {
       servis_count = 0;
@@ -190,6 +266,11 @@ em:
       break;
     }
   }
+  lcd.print(kat); lcd.print(".katta");
+  delay(90);
+  lcd.clear();
+  noTone(7);
+  myservo3.write(0);
 }
 
 void sens()
@@ -197,9 +278,9 @@ void sens()
   ldr1 = analogRead(A1);
   ldr2 = analogRead(A2);
   ldr3 = analogRead(A3);
-  if (ldr1 <= 890)kat = 1;
-  if (ldr2 <= 900)kat = 3;
-  if (ldr3 <= 780)kat = 2;
+  if (ldr1 <= 800)kat = 1;
+  if (ldr2 <= 800)kat = 3;
+  if (ldr3 <= 800)kat = 2;
 }
 void down()
 {
@@ -215,7 +296,8 @@ void down()
     zaman4 = millis();
   }
   katSegment();
-  Serial.println("down");
+
+
 }
 void up()
 {
@@ -230,22 +312,13 @@ void up()
     zaman2 = millis();
   }
   katSegment();
-  Serial.println("UP");
+
+
 }
 void katSegment()
 {
-
   switch (kat)
   {
-    case 0 :
-      digitalWrite(a, LOW);
-      digitalWrite(b, LOW);
-      digitalWrite(c, LOW);
-      digitalWrite(d, LOW);
-      digitalWrite(e, LOW);
-      digitalWrite(f, LOW);
-      digitalWrite(g, HIGH);
-      break;
     case 1:
       digitalWrite(a, HIGH);
       digitalWrite(b, LOW);
@@ -277,78 +350,68 @@ void katSegment()
 }
 void oku()
 {
-  if (digitalRead(10) == 1 && x == 0) {
-    x = 1;
+  if (digitalRead(10) == 1 && x1 == 0) {
+    x1 = 1;
     if (Durum1 == 0) Durum1 = 1;
     else if (Durum1 == 1) Durum1 = 0;
   }
-  else if (digitalRead(10) == 0 && x == 1)x = 0;
+  else if (digitalRead(10) == 0 && x1 == 1)x1 = 0;
 
-  if (digitalRead(12) == 1 && x == 0) {
-    x = 1;
+  if (digitalRead(12) == 1 && x2 == 0) {
+    x2 = 1;
     if (Durum2 == 0) Durum2 = 1;
     else if (Durum2 == 1) Durum2 = 0;
   }
-  else if (digitalRead(12) == 0 && x == 1)x = 0;
+  else if (digitalRead(12) == 0 && x2 == 1)x2 = 0;
 
-  if (digitalRead(9) == 1 && x == 0) {
-    x = 1;
+  if (digitalRead(9) == 1 && x3 == 0) {
+    x3 = 1;
     if (Durum3 == 0) Durum3 = 1;
     else if (Durum3 == 1) Durum3 = 0;
   }
-  else if (digitalRead(9) == 0 && x == 1)x = 0;
+  else if (digitalRead(9) == 0 && x3 == 1)x3 = 0;
 
-  if (digitalRead(53) == 1 && x == 0) {
-    x = 1;
+  if (digitalRead(53) == 1 && x4 == 0) {
+    x4 = 1;
     if (Kabin1 == 0) Kabin1 = 1;
     else if (Kabin1 == 1) Kabin1 = 0;
   }
-  else if (digitalRead(53) == 0 && x == 1)x = 0;
+  else if (digitalRead(53) == 0 && x4 == 1)x4 = 0;
 
-  if (digitalRead(51) == 1 && x == 0) {
-    x = 1;
+  if (digitalRead(51) == 1 && x5 == 0) {
+    x5 = 1;
     if (Kabin2 == 0) Kabin2 = 1;
     else if (Kabin2 == 1) Kabin2 = 0;
   }
-  else if (digitalRead(51) == 0 && x == 1)x = 0;
+  else if (digitalRead(51) == 0 && x5 == 1)x5 = 0;
 
-  if (digitalRead(49) == 1 && x == 0) {
-    x = 1;
+  if (digitalRead(49) == 1 && x6 == 0) {
+    x6 = 1;
     if (Kabin3 == 0) Kabin3 = 1;
     else if (Kabin3 == 1) Kabin3 = 0;
   }
-  else if (digitalRead(49) == 0 && x == 1)x = 0;
+  else if (digitalRead(49) == 0 && x6 == 1)x6 = 0;
 }
 
 void servis()
 {
   servis_count++;
 }
-void emerg()
+/*void emerg()
 {
-  Serial.println("EREN");
+  noTone(7);
+  oku();
+  katSegment();
   while (acil_count > 0)
   {
-    Durum1 = 0;
-    Durum2 = 0;
-    Durum3 = 0;
-    Kabin1 = 0;
-    Kabin2 = 0;
-    Kabin3 = 0;
+    oku();
     sens();
-    down();
-    katSegment();
-    if (ldr1 < 890)
-    {
-      acil_count = 0;
-      Kabin1 = 0;
-      Serial.println("ACİL");
-      break;
+    goto aa; 
+    acil_count = 0;
     }
   }
-}
+*/
 void acil()
 {
   acil_count++;
 }
-
